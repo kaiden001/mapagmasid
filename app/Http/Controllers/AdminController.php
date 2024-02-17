fbb9ff3ca5d9d9f132a2388d87e5a8799eff112d
kaiden001 <116476264+kaiden001@users.noreply.github.com>
2024-02-18 03:30:49 +0800

    update profile with messages

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -33,4 +33,37 @@ class AdminController extends Controller
         $profileData = User::find($id);
         return view('admin.admin_profile_view', compact('profileData'));
     }
+    public function AdminProfileStore(Request $request)
+    {
+        // $data = $request->validate([
+        //     'id' => 'required',
+        //     'name' => 'required',
+        //     'username' => 'required',
+        //     'email' => 'required',
+        //     'phone' => 'required',
+        //     'address' => 'required',
+        //     'photo' => 'required',
+
+        // ]);
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->name = $request->name;
+        $data->username = $request->username;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'), $filename);
+            $data['photo'] = $filename;
+        }
+        $data->save();
+
+        $notification = array(
+            'message' => 'Admin Profile Updated Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+    }
 }
--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -1,5 +1,6 @@
 @extends('admin.admin_dashboard')
 @section('admin')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <div class="page-content">
 
         <div class="row profile-body">
@@ -13,7 +14,7 @@
                                 <img class="wd-100 rounded-circle"
                                     src="{{ !empty($profileData->photo) ? url('upload/admin_images/' . $profileData->photo) : url('upload/no_image.jpg') }}"
                                     alt="profile">
-                                <span class="h4 ms-3">{{ $profileData->username }}</span>
+                                <span class="h4 ms-3">{{ $profileData->name }}</span>
                             </div>
 
                         </div>
@@ -54,31 +55,49 @@
                     <div class="card">
                         <div class="card-body">
 
-                            <h6 class="card-title">Basic Form</h6>
+                            <h6 class="card-title">Update Profile</h6>
 
-                            <form class="forms-sample">
+                            <form class="forms-sample" method="POST" action="{{ route('admin.profile.store') }}"
+                                enctype="multipart/form-data">
+                                @csrf
                                 <div class="mb-3">
-                                    <label for="exampleInputUsername1" class="form-label">Username</label>
-                                    <input type="text" class="form-control" id="exampleInputUsername1" autocomplete="off"
-                                        placeholder="Username">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                                    <label for="name" class="form-label">Name</label>
+                                    <input type="text" name="name" class="form-control" id="name"
+                                        autocomplete="off" placeholder="name" value="{{ $profileData->name }}">
                                 </div>
                                 <div class="mb-3">
-                                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                                    <input type="password" class="form-control" id="exampleInputPassword1"
-                                        autocomplete="off" placeholder="Password">
+                                    <label for="username" class="form-label">Username</label>
+                                    <input type="text" name="username" class="form-control" id="username"
+                                        autocomplete="off" placeholder="Username" value="{{ $profileData->username }}">
                                 </div>
-                                <div class="form-check mb-3">
-                                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                    <label class="form-check-label" for="exampleCheck1">
-                                        Remember me
-                                    </label>
+                                <div class="mb-3">
+                                    <label for="email" class="form-label">Email address</label>
+                                    <input type="email" name="email" class="form-control" id="email"
+                                        placeholder="Email" value="{{ $profileData->email }}">
                                 </div>
-                                <button type="submit" class="btn btn-primary me-2">Submit</button>
-                                <button class="btn btn-secondary">Cancel</button>
+                                <div class="mb-3">
+                                    <label for="phone" class="form-label">Phone</label>
+                                    <input type="text" name="phone" class="form-control" id="phone"
+                                        placeholder="phone" value="{{ $profileData->phone }}">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="address" class="form-label">Address</label>
+                                    <input type="text" name="address" class="form-control" id="address"
+                                        autocomplete="off" placeholder="Address" value="{{ $profileData->address }}">
+                                </div>
+
+                                <div class="mb-3">
+                                    <label class="form-label" for="image">Photo</label>
+                                    <input class="form-control" name="photo" type="file" id="image">
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label" for="showImage"></label>
+                                    <img id="showImage" class="wd-80 rounded-circle"
+                                        src="{{ !empty($profileData->photo) ? url('upload/admin_images/' . $profileData->photo) : url('upload/no_image.jpg') }}"
+                                        alt="profile">
+                                </div>
+                                <button type="submit" class="btn btn-primary me-2">Save Changes</button>
+
                             </form>
 
                         </div>
@@ -89,4 +108,15 @@
         </div>
 
     </div>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $("#image").change(function(e) {
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    $("#showImage").attr('src', e.target.result);
+                }
+                reader.readAsDataURL(e.target.files['0']);
+            })
+        })
+    </script>
 @endsection
