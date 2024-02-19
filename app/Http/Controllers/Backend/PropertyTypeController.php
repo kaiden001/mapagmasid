029c4149d51e198671abcfc8201315ed333335da
kaiden001 <116476264+kaiden001@users.noreply.github.com>
2024-02-19 16:28:17 +0800

    g

---
+++ app/Http/Controllers/Backend/PropertyTypeController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\PropertyType;
+
+class PropertyTypeController extends Controller
+{
+    //
+
+    public function AllType()
+    {
+
+        $types = PropertyType::latest()->get();
+        return view('backend.type.all_type', compact('types'));
+    }
+    public function AddType()
+    {
+        return view('backend.type.add_type');
+    }
+    public function StoreType(Request $request)
+    {
+        $request->validate([
+            'type_name' => 'required|unique:property_types|max:200',
+            'type_icon' => 'required'
+        ]);
+        PropertyType::insert([
+            'type_name' => $request->type_name,
+            'type_icon' => $request->type_icon
+        ]);
+        $notification = array(
+            'message' => 'Property Type Created Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('all.type')->with($notification);
+    }
+    public function EditType($id)
+    {
+        $type = PropertyType::findOrFail($id);
+        return view('backend.type.edit_type', compact('type'));
+    }
+
+    public function UpdateType(Request $request)
+    {
+        $request->validate([
+            'id' => 'required',
+            'type_name' => 'required|unique:property_types|max:200',
+            'type_icon' => 'required'
+        ]);
+        PropertyType::findOrFail($request->id)->update([
+            'type_name' => $request->type_name,
+            'type_icon' => $request->type_icon
+        ]);
+        $notification = array(
+            'message' => 'Property Type Updated Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('all.type')->with($notification);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AgentController;
+use App\Http\Controllers\Backend\PropertyTypeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -46,3 +47,16 @@ Route::middleware(['auth', 'role:agent'])->group(function () {
     Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
 });
 Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+
+
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+
+    Route::controller(PropertyTypeController::class)->group(function () {
+        Route::get('/all/type', 'AllType')->name('all.type');
+        Route::get('/add/type', 'AddType')->name('add.type');
+        Route::post('/store/type', 'StoreType')->name('store.type');
+        Route::get('/edit/type/{id}', 'EditType')->name('edit.type');
+        Route::post('/update/type', 'UpdateType')->name('update.type');
+    });
+});
